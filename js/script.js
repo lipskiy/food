a82c59d1b1060406d1caa2f22ed0b48c46c133f2
Alexander <lipskiypro@gmail.com>
2022-07-13 21:50:43 +0300

    modal v1

--- js/script.js
+++ js/script.js
@@ -100,4 +100,49 @@ window.addEventListener('DOMContentLoaded', () => {
    }
 
    setClock('.timer', deadline);
+
+   // Modal 
+
+   // Сначала объявляем переменные.
+   const modalTrigger = document.querySelectorAll('[data-modal]'),
+         modal = document.querySelector('.modal'),
+         modalCloseBtn = document.querySelector('[data-close]');
+
+   // Алгоритм такой. Понадобится всего 2 функции. Одна отвечает за открытие модалки, вторая за закрытие. Ну и нужно подвязать на несколько триггеров, чтобы функция срабатывала и на всех кнопках. Так как мы не можем на пвсевдомассив навесить обработчик событий, то нам просто нужно перебрать все кнопки через forEach.
+
+   // Мы при клике добавляем класс "показать" и убираем класс "скрыть". Через оверфлоу убрал скролл страницы при открытом модальном окне. Можно через toggle. 
+
+   modalTrigger.forEach(btn => {
+      btn.addEventListener('click', () => {
+         modal.classList.add('show');
+         modal.classList.remove('hide');
+         // modal.classList.toggle('show');
+         document.body.style.overflow = 'hidden';
+      });
+   });
+
+   // Так как у нас переиспользуется функция закрытия модального окна, то мы выносим ее в отдельную функцию, чтобы оптимизировать код.
+   function closeModal () {
+      modal.classList.add('hide');
+      modal.classList.remove('show');
+      // modal.classList.toggle('show');
+      document.body.style.overflow = '';
+   }
+
+   modalCloseBtn.addEventListener('click', closeModal);
+
+   // Для того чтобы модалку можно было закрыть кликнув на область страницы используем обрабочтик событий и следим куда кликнул пользователь.
+   modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+         closeModal();
+      }
+   });
+
+   // Чтобы закрыть модалку на нажатие кнопки ESC на клавиатуре. Для этого используем событие "keydown". Для этого мы проверяем код клавиши, на которую нажали. Для того чтобы Escape срабатывал только тогда, когда открыто модальное окно мы это прописываем в условии.
+   document.addEventListener('keydown', (e) => {
+      if (e.code === "Escape" && modal.classList.contains('show')) {
+         closeModal();
+      }
+   });
+
 });
