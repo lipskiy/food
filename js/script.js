3f1f0f35d856115b8751f8f7e3d4f067182081af
Alexander <lipskiypro@gmail.com>
2022-07-17 01:52:37 +0300

    используем классы для шаблонизации

--- js/script.js
+++ js/script.js
@@ -149,7 +149,7 @@ window.addEventListener('DOMContentLoaded', () => {
    });
 
    //Чтобы модалка появлялась через опредленный промежуток времени сама. Используем setTimeout.
-   const modalTimerId = setTimeout(openModal, 5000);
+   // const modalTimerId = setTimeout(openModal, 15000);
 
    //Чтобы модалка открывалась, когда пользователь долистал страницу до конца. Мы можем использовать событие scroll и вешается глобально на window.  Логика: когда мы берем контент, который уже проскролили и видимый контент и при сложении они совпадают с полной прокруткой сайта, то значит пользователь долистал до конца. Тк был обнаружен баг, что модалка не видна, то в конце использовался минус 1 пиксель. Тк каждый раз модалка появляется при скролле до конца, то используем удаление обработчика события. Чтобы удалить обработчик события, то мы должны делать ссылку на функцию, которая исполнялась.
    function showModalByScroll () {
@@ -160,4 +160,71 @@ window.addEventListener('DOMContentLoaded', () => {
    }
 
    window.addEventListener('scroll', showModalByScroll);
+
+   // Используем классы для карточек 
+
+   // Создаем класс и переменные, которые нам понадобятся 
+   class MenuCard {
+      constructor(src, alt, title, descr, price, parentSelector) {
+         this.src = src;
+         this.alt = alt;
+         this.title = title;
+         this.descr = descr;
+         this.price = price;
+         this.parent = document.querySelector(parentSelector);
+         this.transfer = 27;
+         this.changeToUAH();
+      }
+
+      // Метод для конвертации валюты
+      changeToUAH() {
+         this.price = this.price * this.transfer;
+      }
+
+      // Создаем динамическую верстку. 
+      render() {
+         const element = document.createElement('div');
+         element.innerHTML = `
+            <div class="menu__item">
+               <img src=${this.src} alt=${this.alt}>
+               <h3 class="menu__item-subtitle">${this.title}</h3>
+               <div class="menu__item-descr">${this.descr}</div>
+               <div class="menu__item-divider"></div>
+               <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+               </div>
+            </div>
+         `;
+         this.parent.append(element);
+      }
+   }
+
+   new MenuCard(
+      "img/tabs/vegy.jpg",
+      "vegy",
+      'Меню "Фитнес"',
+      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+      9,
+      ".menu .container"
+   ).render();
+
+   new MenuCard(
+      "img/tabs/elite.jpg",
+      "elite",
+      'Меню “Премиум”',
+      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+      14,
+      ".menu .container"
+   ).render();
+
+   new MenuCard(
+      "img/tabs/post.jpg",
+      "post",
+      'Меню "Постное"',
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+      21,
+      ".menu .container"
+   ).render();
+
 });
