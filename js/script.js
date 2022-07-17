d6c14c88cfde90173cd27cba4474de8d439eebfd
Alexander <lipskiypro@gmail.com>
2022-07-17 13:48:05 +0300

    отправляем данные на сервер

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
    // Tabs
 
+   // Получаем переменные, с которыми мы будем взаимодействовать. Будем использовать делигирование событий. 
    const tabs = document.querySelectorAll('.tabheader__item'),
          tabsContent = document.querySelectorAll('.tabcontent'),
          tabsParent = document.querySelector('.tabheader__items');
 
+   // Скрываем все ненужные нам Табы. Перебираем псевдомассив. Добавляем класс hide и удаляем классы show and fade. Это делается для анимации табов.
    function hideTabContent() {
       tabsContent.forEach(item => {
          item.classList.add('hide');
          item.classList.remove('show', 'fade');
       });
 
+      // У каждого из элементов Таба удаляем класс активности.
       tabs.forEach(item => {
          item.classList.remove('tabheader__item_active');
       });   
    }
 
+   // Эта функция показывает нам нужные табы. И добавлем класс активности. i = 0 - это позволяет нам показывать первый таб. Используется для этого параметр по умолчанию. Такая фозможность появилась в стандарте ES6. Добавляем класс show and fade и удаляем классы hide. Это делается для анимации табов.
    function showTabContent(i = 0) {
       tabsContent[i].classList.add('show', 'fade');
       tabsContent[i].classList.remove('hide');
@@ -25,9 +29,11 @@ window.addEventListener('DOMContentLoaded', () => {
    hideTabContent();
    showTabContent();
 
+   // Используем делигирование события. Используем обработчик события. 
    tabsParent.addEventListener('click', (event) => {
-      const target = event.target;
+      const target = event.target; // Это если нам нужно все время использовать event.target. 
 
+      // Определяем точно ли мы кликнули в таб. Перебираем все эелементы в псевдомассиве, чтобы понять в какой мы клацнули и какой нужно показать. Два аргумента: таб и номер элемента по порядку. Если эелемент, в который мы кликнули, будет совпадать с элементом, который мы перебираем, то мы вызываем две наши функции.
       if (target && target.classList.contains('tabheader__item')) {
          tabs.forEach((item, i) => {
             if (target == item) {
@@ -233,5 +239,57 @@ window.addEventListener('DOMContentLoaded', () => {
       21,
       ".menu .container"
    ).render();
+
+   // Forms
+
+   // Получаем все формы, которые у нас есть.
+   const forms = document.querySelectorAll('form');
+
+   // Временно создаем список фраз для оповщения пользователя. 
+   const message = {
+      loading: 'Загрузка...',
+      success: 'Спасибо! Скоро мы с Вами свяжемся',
+      failure: 'Что то пошло не так...'
+   };
+
+   // Берем все наши формы и под каждую из них подвязываем нашу postDate.
+   forms.forEach(item => {
+      postData(item);
+   });
+
+   // Создаем функцию, которая будет отвечать за постинг данных. Передаем форму, потому что удобно взять и навесить обработчик событий. Submit - срабатывает каждый раз, когда мы пытаемся отправить какую то форму. Element - нужен для того, чтобы отменить стандартное поведение браузера (перезагрузку страницы).
+   function postData(form) {
+      form.addEventListener('submit', (e) => {
+         e.preventDefault();
+
+         // Динамически создаем новый блок для вывода сообщения для пользователя.
+         const statusMessage = document.createElement('div');
+         statusMessage.classList.add('status');
+         statusMessage.textContent = message.loading;
+         form.appendChild(statusMessage);
+
+         const request = new XMLHttpRequest();
+         request.open('POST', 'server.php'); // Для настройки запроса.
+
+         // request.setRequestHeader('Content-type', 'multipart/form-data'); // Настройка заголовков. Когда используем связку XMLHttpRequest и form-data нам заголовок устанавливать не нужно. Он устанавливется автоматически. Из за такой ошибки сервер может не получить от нас данные.ЭТО ВАЖНО!
+         // request.setRequestHeader('Content-type', 'application/json'); // Если вдруг нам нужен формат JSON.
+         const formData = new FormData(form); // Специальный объект, который позволяет с определенной формы сформировать данные, которые заполнил пользователь. Формат: ключ - значение.
+
+         request.send(formData); // Отправка данных с помощью метода send.
+
+         request.addEventListener('load', () => {
+            if (request.status === 200) {
+               console.log(request.response);
+               statusMessage.textContent = message.success;
+               form.reset(); // Очистка формы
+               setTimeout(() => {
+                  statusMessage.remove();
+               }, 2000); // Удаляет блок с сообщением со страницы.
+            } else {
+               statusMessage.textContent = message.failure;
+            }
+         }); // Отслеживаем конечную загрузку нашего запроса.
+      });
+   }
 
 });
