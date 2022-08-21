981d73de4090fb1bd092d14ec72a123e5a10fcb8
Alexander <lipskiypro@gmail.com>
2022-08-21 17:56:37 +0300

    async/await

--- js/script.js
+++ js/script.js
@@ -220,34 +220,25 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    }
 
-   new MenuCard(
-      "img/tabs/vegy.jpg",
-      "vegy",
-      'Меню "Фитнес"',
-      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-      9,
-      ".menu .container"
-   ).render();
+   const getResource = async (url) => {
+      const res = await fetch(url);
 
-   new MenuCard(
-      "img/tabs/elite.jpg",
-      "elite",
-      'Меню “Премиум”',
-      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-      14,
-      ".menu .container"
-   ).render();
+      if(!res.ok) {
+         throw new Error(`Could not fetch ${url}, status: ${res.status}`); // Логика: если что то пошло не так, то мы выкидываем (throw) новую ошибку. 
+      }
 
-   new MenuCard(
-      "img/tabs/post.jpg",
-      "post",
-      'Меню "Постное"',
-      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-      21,
-      ".menu .container"
-   ).render();
+      return await res.json();
+   };
 
-   // Forms
+   getResource('http://localhost:3000/menu')
+      .then(data => {
+         data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+         });
+      });
+
+
+   // Forms =================================================================================================>>
 
    // Получаем все формы, которые у нас есть.
    const forms = document.querySelectorAll('form');
@@ -261,11 +252,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
    // Берем все наши формы и под каждую из них подвязываем нашу postDate.
    forms.forEach(item => {
-      postData(item);
+      bindPostData(item);
    });
 
+
+   // Логика: ф-ия настраивает запрос, посылает запрос на сервер, получает какой то ответ и трансформирует ответ в json.  
+   const postData = async (url, data) => {
+      const res = await fetch(url, {
+         method: "POST",
+         headers: {
+            'Content-type': 'application/json'
+         },
+         body: data
+      });
+
+      return await res.json();
+   };
+
    // Создаем функцию, которая будет отвечать за постинг данных. Передаем форму, потому что удобно взять и навесить обработчик событий. Submit - срабатывает каждый раз, когда мы пытаемся отправить какую то форму. Element - нужен для того, чтобы отменить стандартное поведение браузера (перезагрузку страницы).
-   function postData(form) {
+   function bindPostData(form) {
       form.addEventListener('submit', (e) => {
          e.preventDefault();
 
@@ -280,19 +285,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
          const formData = new FormData(form); // Специальный объект, который позволяет с определенной формы сформировать данные, которые заполнил пользователь. Формат: ключ - значение.
 
-         const object = {};
-         formData.forEach(function(value, key){
-            object[key] = value;
-         });
+         // Логика: берем наши данные, сначала превращаем в массив массивов, затем после превращаем в классический объект (fromEntries), а после этот объект превращаем в json. 
+         const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-         fetch('server.php', {
-            method: "POST",
-            headers: {
-               'Content-type': 'application/json'
-            },
-            body: JSON.stringify(object)
-         })
-         .then(data => data.text())
+         postData('http://localhost:3000/requests', json)
          .then(data => {
             console.log(data);
             showThanksModal(message.success);
@@ -328,9 +324,4 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-
-    fetch('http://localhost:3000/menu')
-      .then(data => data.json())
-      .then(res => console.log(res));
-
 });
